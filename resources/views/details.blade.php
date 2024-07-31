ac3d92fc4b7eddad6de7ae8808aa8aa219e1ef19
Jan Suchánek <jan.suchanek@gmail.com>
2024-07-31 14:04:48 +0200

    Update details.blade.php

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -14,7 +14,7 @@
             <h5 class="card-title">{{$todos->name}}</h5>
             <p class="card-text">{{$todos->description}}.</p>
             <a href="/edit/{{$todos->id}}"><span class="btn btn-primary">Upravit</span></a>
-            <a href="/delete/{{$todos->id}}"><span class="btn btn-danger">Smaza</span></a>
+            <a href="/delete/{{$todos->id}}"><span class="btn btn-danger">Smazat</span></a>
         </div>
     </div>
 
